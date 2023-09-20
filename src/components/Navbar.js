9633248f61dba02cb58ce600febda119db3e2ab8
unknown <ismayilrha@code.edu.az>
2023-09-20 19:01:29 +0400

    added last version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,28 @@
-import React from "react";
-import logo from "../images/logo.svg";
-import { pageLinks } from "../data";
-import { socialLinks } from "../data";
-function Navbar() {
+import logo from '../images/logo.svg'
+import { pageLinks, socialLinks } from '../data'
+import PageLinks from './PageLinks'
+import SocialLink from './SocialLink'
+const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="nav-center">
-        <div className="nav-header">
-          <img src={logo} className="nav-logo" alt="backroads" />
-          <button type="button" className="nav-toggle" id="nav-toggle">
-            <i className="fas fa-bars"></i>
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='backroads' />
+          <button type='button' className='nav-toggle' id='nav-toggle'>
+            <i className='fas fa-bars'></i>
           </button>
         </div>
-        {/* {  <!-- left this comment on purpose -->} */}
-        <ul className="nav-links" id="nav-links">
-          {pageLinks.map((link) => (
-            <li key={link.id}>
-              <a href={link.href} className="nav-link">
-                  {link.text}
-              </a>
-            </li>
-          ))}
-        </ul>
+        {/* <!-- left this comment on purpose --> */}
 
-        <ul className="nav-icons">
-          {socialLinks.map(({id,href,icon}) => {
-            return(
-              <li key={id}>
-            <a
-              href={href}
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className={icon}></i>
-            </a>
-          </li>
-            )
+        <PageLinks parentClass='nav-links' itemClass='nav-link' />
+
+        <ul className='nav-icons'>
+          {socialLinks.map((link) => {
+            return <SocialLink {...link} key={link.id} itemClass='nav-icon' />
           })}
         </ul>
       </div>
     </nav>
-  );
+  )
 }
-
-export default Navbar;
+export default Navbar
